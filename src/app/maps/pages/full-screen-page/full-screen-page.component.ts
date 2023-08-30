d91dcf03756249f2590d80aed66de010dd32e2fc
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-30 16:18:56 +0200

    Mostrar el mapa en modo pantalla completa

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import * as  mapboxgl from 'mapbox-gl'
+(mapboxgl as any).accessToken = "pk.eyJ1IjoibWFyaW9hZzEiLCJhIjoiY2xseGx0dGt2MDNuczNxbWhwazF6NzEzOSJ9.7rhWGA5qKEO0NG6XFnGk5g"
+
 
 @Component({
   selector: 'app-full-screen-page',
   templateUrl: './full-screen-page.component.html',
-  styles: [
+  styleUrls: [ './full-screen-page.component.css'
   ]
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    })
+  }
 }
