4e6b17779b19519ad745bd6f60fdac5ac9233ee2
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-30 12:01:27 +0200

    Creacion del menu de la aplicacion

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  route: string,
+  name: string
+}
+
 @Component({
-  selector: 'app-side-menu',
+  selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
-  styles: [
-  ]
+  styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent {
 
+  public menuItems: MenuItem[] = [
+    {
+      route: "/maps/fullscreen", name: "FullScreen"
+    },
+    {
+      route: "/maps/zoom-range", name: "ZoomRange"
+    },
+    {
+      route: "/maps/markers", name: "Markers"
+    },
+    {
+      route: "/maps/properties", name: "House"
+    },
+  ]
 }
+
+
