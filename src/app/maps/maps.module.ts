3c0de3774eec587388b8a7beb8eefebb4aca5e0e
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-30 16:58:07 +0200

    ViewChild - ElementRef

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import * as  mapboxgl from 'mapbox-gl'
+(mapboxgl as any).accessToken = "pk.eyJ1IjoibWFyaW9hZzEiLCJhIjoiY2xseGx0dGt2MDNuczNxbWhwazF6NzEzOSJ9.7rhWGA5qKEO0NG6XFnGk5g"
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,20 +1,23 @@
-import { AfterViewInit, Component } from '@angular/core';
-
-import * as  mapboxgl from 'mapbox-gl'
-(mapboxgl as any).accessToken = "pk.eyJ1IjoibWFyaW9hZzEiLCJhIjoiY2xseGx0dGt2MDNuczNxbWhwazF6NzEzOSJ9.7rhWGA5qKEO0NG6XFnGk5g"
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl'
 
 
 @Component({
   selector: 'app-full-screen-page',
   templateUrl: './full-screen-page.component.html',
-  styleUrls: [ './full-screen-page.component.css'
+  styleUrls: ['./full-screen-page.component.css'
   ]
 })
 export class FullScreenPageComponent implements AfterViewInit {
 
+  @ViewChild('map') divmap?: ElementRef
+
   ngAfterViewInit(): void {
-    const map = new mapboxgl.Map({
-      container: 'map', // container ID
+
+    if (!this.divmap) throw "El elemento no fue encontrado"
+
+    const map = new Map({
+      container: this.divmap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
