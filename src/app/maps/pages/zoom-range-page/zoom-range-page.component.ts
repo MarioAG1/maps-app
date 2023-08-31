605e908255b44b3b166121f6c2bcc60d4c51099c
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-31 15:34:06 +0200

    Diseño ZoomIn y ZoomOut

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl'
 
 @Component({
   selector: 'app-zoom-range-page',
   templateUrl: './zoom-range-page.component.html',
-  styles: [
+  styleUrls: ['./zoom-range-page.component.css'
   ]
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit {
+
+  @ViewChild('map') divmap?: ElementRef
+
+  ngAfterViewInit(): void {
+
+    if (!this.divmap) throw "El elemento no fue encontrado"
+
+    const map = new Map({
+      container: this.divmap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    })
+  }
 
 }
